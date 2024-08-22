5740112ddd6ed782a31fc4198a47a33cf61db28d
Raposobl <leonardoraposoboechat@gmail.com>
2024-08-21 22:29:47 -0300

    esse vai ser só um commit teste

--- js/controlStructure.js
+++ js/controlStructure.js
@@ -6,3 +6,19 @@ if(se == true){
     console.log(se)
 }
 //else é a parte da negativa da estrutura if, ela executa caso o if retorne "falso" para a condição descrita
+se = false
+if(se == true){
+}else{
+    console.log(se)
+}
+//else if é uma estrutura que deriva do conceito if else, que vai permitir que sejam verificadas mais condições dentro da estrutura if
+
+se = false
+
+if (se == true){
+    console.log(se)
+}else if(se =="a"){
+    console.log(se)
+}else{
+    console.log(se)
+}
