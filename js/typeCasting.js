e6a34d4d6c46f19fdde138a0871187d59c4413e2
Raposobl <leonardoraposoboechat@gmail.com>
2024-08-26 20:49:52 -0300

    finalização do Type Casting

--- js/typeCasting.js
+++ js/typeCasting.js
@@ -1,5 +1,8 @@
 // Type Casting ou Coerção de Tipo, em js é o processo de converter um tipo de dado em outro
 
+//Type Conversion VS Type Coercion
+// Conversão de Tipo é quando de maneira manual o programador faz a conversão de um tipo de dado para outro
+
 let um = 1
 let dois = "2"
 console.log(typeof um ) //Number
@@ -9,7 +12,7 @@ console.log(typeof dois) //String
 console.log(typeof String(um))
 console.log(typeof Number(dois))
 
-// Coerção implicita no javaScript existe a coerção implicita, que é a conversão de dados feito pelo proprio javaScript, no caso ele faz essa coerção para determinadas operações como por exemplo tentar multiplicar uma string com um numero, se o valor da string for um numero o java script faz essa coerção automaticamente
+// Coerção Implicita no javaScript existe a coerção implicita, que é a conversão de dados feito pelo proprio javaScript, no caso ele faz essa coerção para determinadas operações como por exemplo tentar multiplicar uma string com um numero, se o valor da string for um numero o java script faz essa coerção automaticamente
 
 let concatenaString = "5" +3
 //Soma com Strings: Quando você usa o operador + com uma string, o JavaScript converte o outro operando para uma string e concatena os dois valores.
